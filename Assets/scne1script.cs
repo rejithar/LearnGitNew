0f43ad42f2ae9b3bcbb607b7371bef4c267e00b2
Rejitha <rejitha@evelynsoftwarelabs.com>
2012-08-03 12:29:14 +0530

    added Image and sound and script to Scn1

---
+++ Assets/scne1script.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class scne1script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
